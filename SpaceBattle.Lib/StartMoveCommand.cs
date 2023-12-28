a187cd5e6685e9b7b5e893d9fabfccbe8ba9ac59
AAArinAAA <15days.before@gmail.com>
2023-12-29 03:27:50 +0600

    нельзя было ничего менять в исходном коде!

--- SpaceBattle.Lib/StartMoveCommand.cs
+++ SpaceBattle.Lib/StartMoveCommand.cs
@@ -12,9 +12,9 @@ namespace SpaceBattle.Lib
         public void Execute()
         {
             moveCommandStartable.property.ToList().ForEach(p => moveCommandStartable.target.SetProperty(p.Key, p.Value));
-            var command = IoC.Resolve<ICommand>("Game.Commands.Move", moveCommandStartable.target);
-            var injectable = IoC.Resolve<ICommand>("Game.Commands.Injectable", command);
-            moveCommandStartable.target.SetProperty("Сommand", command);
+            var command = IoC.Resolve<ICommand>("Game.Operation.Move", moveCommandStartable.target);
+            var injectable = IoC.Resolve<ICommand>("Commands.Injectable", command);
+            moveCommandStartable.target.SetProperty("command", command);
             IoC.Resolve<IQueue>("Game.Queue", injectable).Enqueue(command);
         }
     }
