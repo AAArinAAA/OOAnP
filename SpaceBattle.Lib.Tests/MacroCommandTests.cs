d68ccf3b1c50cfa11ca6693d1efb500050881c31
AAArinAAA <15days.before@gmail.com>
2023-12-28 18:44:46 +0600

    all good

---
+++ SpaceBattle.Lib.Tests/MacroCommandTests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using Hwdtech;
+using Hwdtech.Ioc;
+using Moq;
+using Xunit;
+
+namespace SpaceBattle.Lib.Tests;
+
+public class MacroCommandTest
+{
+    public MacroCommandTest()
+    {
+        new InitScopeBasedIoCImplementationCommand().Execute();
+        IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Root"))).Execute();
+
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.Command.CreateMacroCommand", (object[] args) =>
+        {
+            var commands = (IEnumerable<ICommand>)args[0];
+            return new MacroCommand(commands);
+        }).Execute();
+
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.Strategy.MacroCommand", (object[] args) =>
+        {
+            var nameOp = (string)args[0];
+            var uObj = (IUObject)args[1];
+            return new MacroCommandStrategy().Init(nameOp, uObj);
+        }).Execute();
+    }
+
+    [Fact]
+    public void SuccessfulExampleOfCreatingAndRunningMacroCommand()
+    {
+        var nameOperation = "MovementAndRotationOperation";
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Component" + nameOperation, (object[] args) =>
+        new string[] { "Game.Command.CreateMoveCommand", "Game.Command.CreateTurnCommand" }).Execute();
+
+        var obj = new Mock<IUObject>();
+
+        var moveCommand = new Mock<ICommand>();
+        moveCommand.Setup(x => x.Execute()).Verifiable();
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.Command.CreateMoveCommand", (object[] args) => moveCommand.Object).Execute();
+
+        var turnCommand = new Mock<ICommand>();
+        turnCommand.Setup(x => x.Execute()).Verifiable();
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.Command.CreateTurnCommand", (object[] args) => turnCommand.Object).Execute();
+
+        var macroCommand = IoC.Resolve<ICommand>("Game.Strategy.MacroCommand", nameOperation, obj.Object);
+
+        macroCommand.Execute();
+
+        moveCommand.Verify(x => x.Execute(), Times.Once);
+        turnCommand.Verify(x => x.Execute(), Times.Once);
+    }
+
+    [Fact]
+    public void TryExecuteCommandsInMacroCommandThrowException()
+    {
+        var command1 = new Mock<ICommand>();
+        command1.Setup(x => x.Execute()).Throws(new Exception());
+
+        var command2 = new Mock<ICommand>();
+        command2.Setup(x => x.Execute()).Verifiable();
+
+        var commands = new List<ICommand> { command1.Object, command2.Object };
+
+        Assert.Throws<Exception>(() => new MacroCommand(commands).Execute());
+        command2.Verify(x => x.Execute(), Times.Never);
+    }
+}
