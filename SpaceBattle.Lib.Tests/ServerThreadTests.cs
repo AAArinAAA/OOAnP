94253d06e70d64edb43218bf83b3d64158b08483
AAArinAAA <15days.before@gmail.com>
2024-03-06 20:48:23 +0600

    после лабы

--- SpaceBattle.Lib.Tests/ServerThreadTests.cs
+++ SpaceBattle.Lib.Tests/ServerThreadTests.cs
@@ -1,14 +1,84 @@
-﻿using Hwdtech;
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using Hwdtech;
+using Hwdtech.Ioc;
+using Moq;
 using Xunit;
 
 namespace SpaceBattle.Lib.Tests;
 
+public class ActionCommand: ICommand 
+{
+    private readonly Action _action;
+    public ActionCommand(Action action) 
+    {
+        _action = action;
+    }
+
+    public void Execute()
+    {
+        _action();
+    }
+}
+
 public class ServerThreadTests
 {
+public ServerThreadTests() 
+{
 
-    [Fact]
-    public static void PositiveTest()
+    new InitScopeBasedIoCImplementationCommand().Execute();
+
+    IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "HardStop",
+        (object[] args) => {
+            var thread = (ServerThread)args[0];
+            var action = (Action)args[1]; 
+            return new ActionCommand (
+                () => {
+                    new HardStop(thread).Execute();
+                    new ActionCommand(action).Execute();
+                }
+            );
+        }
+    ).Execute();
+}
+
+     [Fact]
+    public void HardStopShouldStopServerThread()
     {
 
+        IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", IoC.Resolve<object>("Scopes.Root"));
+        
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "ExceptionHandler", (object[] args) => new ActionCommand(() => {})).Execute();
+        
+       
+
+        var q = new BlockingCollection<ICommand>(10);
+        var st = new ServerThread(q);
+
+        var command = new Mock<ICommand>();
+        command.Setup(m => m.Execute()).Verifiable();
+
+        q.Add(command.Object);
+        var mre = new ManualResetEvent(false);
+
+        
+        IoC.Resolve<Hwdtech.ICommand>(
+            "IoC.Register", 
+            "HardStopCommand", 
+            (object[] args) => new ActionCommand(() => {mre.Set();})
+        ).Execute();
+   
+
+        q.Add(IoC.Resolve<ICommand>("HardStopCommand"));
+
+
+        q.Add(command.Object);
+        
+        st.Start();
+        Assert.True(mre.WaitOne(1000));
+
+        Assert.Single(q);
+        command.Verify(m=> m.Execute(), Times.Once);
     }
 }
