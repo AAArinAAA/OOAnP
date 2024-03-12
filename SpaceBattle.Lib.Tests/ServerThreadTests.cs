dd8119e6a0e980ff1695d5fe3f536d0362f86546
AAArinAAA <15days.before@gmail.com>
2024-03-12 23:59:24 +0600

    dotnet format

--- SpaceBattle.Lib.Tests/ServerThreadTests.cs
+++ SpaceBattle.Lib.Tests/ServerThreadTests.cs
@@ -8,10 +8,10 @@ using Xunit;
 
 namespace SpaceBattle.Lib.Tests;
 
-public class ActionCommand: ICommand 
+public class ActionCommand : ICommand
 {
     private readonly Action _action;
-    public ActionCommand(Action action) 
+    public ActionCommand(Action action)
     {
         _action = action;
     }
@@ -24,34 +24,34 @@ public class ActionCommand: ICommand
 
 public class ServerThreadTests
 {
-public ServerThreadTests() 
-{
+    public ServerThreadTests()
+    {
 
-    new InitScopeBasedIoCImplementationCommand().Execute();
+        new InitScopeBasedIoCImplementationCommand().Execute();
 
-    IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "HardStop",
-        (object[] args) => {
-            var thread = (ServerThread)args[0];
-            var action = (Action)args[1]; 
-            return new ActionCommand (
-                () => {
-                    new HardStop(thread).Execute();
-                    new ActionCommand(action).Execute();
-                }
-            );
-        }
-    ).Execute();
-}
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "HardStop",
+            (object[] args) =>
+            {
+                var thread = (ServerThread)args[0];
+                var action = (Action)args[1];
+                return new ActionCommand(
+                    () =>
+                    {
+                        new HardStop(thread).Execute();
+                        new ActionCommand(action).Execute();
+                    }
+                );
+            }
+        ).Execute();
+    }
 
-     [Fact]
+    [Fact]
     public void HardStopShouldStopServerThread()
     {
 
         IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", IoC.Resolve<object>("Scopes.Root"));
-        
-        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "ExceptionHandler", (object[] args) => new ActionCommand(() => {})).Execute();
-        
-       
+
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "ExceptionHandler", (object[] args) => new ActionCommand(() => { })).Execute();
 
         var q = new BlockingCollection<ICommand>(10);
         var st = new ServerThread(q);
@@ -62,23 +62,21 @@ public ServerThreadTests()
         q.Add(command.Object);
         var mre = new ManualResetEvent(false);
 
-        
         IoC.Resolve<Hwdtech.ICommand>(
-            "IoC.Register", 
-            "HardStopCommand", 
-            (object[] args) => new ActionCommand(() => {mre.Set();})
+            "IoC.Register",
+            "HardStopCommand",
+            (object[] args) => new ActionCommand(() => { mre.Set(); })
         ).Execute();
-   
 
         q.Add(IoC.Resolve<ICommand>("HardStopCommand"));
 
         q.Add(command.Object);
-        
+
         st.Start();
-        
+
         Assert.True(mre.WaitOne(1000));
 
         Assert.Empty(q);
-        command.Verify(m=> m.Execute(), Times.AtLeast(2));
+        command.Verify(m => m.Execute(), Times.AtLeast(2));
     }
 }
