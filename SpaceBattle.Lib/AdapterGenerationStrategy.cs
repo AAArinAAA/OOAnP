b78e90554d43d383e7283805ddd82cc052fe514c
AAArinAAA <15days.before@gmail.com>
2024-05-29 12:15:07 +0600

    adapters

---
+++ SpaceBattle.Lib/AdapterGenerationStrategy.cs
@@ -0,0 +1,22 @@
+using System.Reflection;
+namespace SpaceBattle.Lib;
+
+public class AdapterCodeGeneratorStrategy : IStrategyRenamed
+{
+    public object Strategy(params object[] args)
+    {
+        var adapterType = (Type)args[0];
+        var targetType = (Type)args[1];
+
+        var builder = new AdapterBuilder(adapterType, targetType);
+
+        var properties = adapterType.GetProperties(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+
+        foreach (var p in properties)
+        {
+            builder.CreateProperty(p);
+        }
+
+        return builder.Build();
+    }
+}
