aa1fa81a88e2ad79e7fb3d3ce935274813fae204
AAArinAAA <15days.before@gmail.com>
2024-05-29 11:49:42 +0600

    lab9

---
+++ SpaceBattle.Lib/Position.cs
@@ -0,0 +1,23 @@
+namespace SpaceBattle.Lib;
+
+public class PosIterGetAndMove : IStrategyRenamed
+{
+    private readonly IEnumerator<object> poit;
+
+    public PosIterGetAndMove(IEnumerator<object> poit)
+    {
+        this.poit = poit;
+    }
+
+    public object Strategy(params object[] args)
+    {
+        var c = (Vector)poit.Current;
+        var m = poit.MoveNext();
+        if (!m) 
+        {
+            poit.Reset();
+        }
+
+        return c;
+    }
+}
---
+++ SpaceBattle.Lib/SetPositionStrategy.cs
@@ -0,0 +1,10 @@
+namespace SpaceBattle.Lib;
+
+public class SetPositionStrategy : IStrategyRenamed
+{
+    public object Strategy(params object[] args)
+    {
+        var patient = (IUObject) args[0];
+        return new SetPoitionCommand(patient);
+    }
+}
